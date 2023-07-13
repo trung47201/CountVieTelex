6f7d3530608202ce0dfdc0db4f9fdf4503cdc0df
Trung <101633153+trung47201@users.noreply.github.com>
2023-07-13 22:31:33 +0700

    upload

---
+++ feature/algorithm/Demo.java
@@ -0,0 +1,39 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+public class Demo {
+	private static final List<String> listOne = Arrays.asList("aw", "aa", "dd", "ee", "oo", "ow", "w");
+
+	public static int countVieLettersTelex(String strs) {
+		int count = 0;
+		String newStrs = strs.replaceAll("\\s+", "");
+		for (String it : listOne) {
+			while (!newStrs.replaceFirst(it, "").equals(newStrs)) {
+				newStrs = newStrs.replaceFirst(it, "");
+				count++;
+			}
+		}
+		return count;
+	}
+
+	public static boolean isAlpha(String strs) {
+		return strs.matches("[a-zA-Z0-9]+");
+	}
+
+	public static boolean input() {
+		Scanner scan = new Scanner(System.in);
+		String strs = scan.nextLine();
+		if (isAlpha(strs)) {
+			System.out.println(countVieLettersTelex(strs));
+			return true;
+		}
+		return false;
+	}
+
+	public static void main(String[] args) {
+		while (!input()) {
+			System.out.println("Vui lòng nhập lại (Chuỗi k được chưa ký tự đặc biệt hoặc khoảng trắng).");
+		}
+	}
+}
